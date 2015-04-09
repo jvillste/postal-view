dd699dae3cb78c5c5225ca65564dd20ad043f6df
Jukka Villstedt <jukka.villstedt@gmail.com>
2015-04-09 22:51:03 +0300

    added data.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [org.clojure/data.csv "0.1.2"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler postal-view.core/handler}
   :main postal-view.main)
